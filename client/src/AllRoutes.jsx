adbdb66b530d86778e20bd5334a7982bc8a4d80d
SouZe-San <2003soumyajit@gmial.com>
2023-05-27 00:49:07 +0530

    Ask Question partially done

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -4,11 +4,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Authenticate from "./pages/Auth/Authenticate";
 import Home from "./pages/Home/Home";
+import AskQuestion from "./pages/AskQuestoin/AskQuestion";
+
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Auth" element={<Authenticate />} />
+      <Route path="/AskQuestion" element={<AskQuestion />} />
     </Routes>
   );
 };
