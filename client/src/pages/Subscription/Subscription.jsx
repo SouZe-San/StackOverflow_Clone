ee2a3f8caa0decf79246eb51b326492189d1f0b0
SouZe-San <2003soumyajit@gmial.com>
2023-07-16 11:51:43 +0530

    Api added

--- client/src/pages/Subscription/Subscription.jsx
+++ client/src/pages/Subscription/Subscription.jsx
@@ -27,7 +27,7 @@ const Subscription = () => {
         image:
           "https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
         order_id: data.id,
-        callback_url: "http://localhost:5000/payment/verification",
+        callback_url: "https://stackoverflowbackend-oucf.onrender.com/payment/verification",
         prefill: {
           name: User?.name,
           email: User?.email,
